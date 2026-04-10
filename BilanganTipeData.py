015912822213e8cfe4b03cffb5b422e51ae6b504
Haryakbar3112 <haryakbar2006@gmail.com>
2026-04-10 10:28:13 +0700

    add

---
+++ BilanganTipeData.py
@@ -0,0 +1,7 @@
+# Tampilan variabel dengan tipe data desimal, octal dan hexadecimal
+angka = 15
+print("angka Desimal:", angka, "-", type(angka))
+print("angka Octal:", oct(angka), "-", type(oct(angka)))
+print("angka Hexadesimal:", hex(angka), "-", type(hex(angka)))
+
+print("----------------------------------------------------------")
